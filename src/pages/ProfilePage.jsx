6d64ac4233d123efcf9c79051d789236eba2f437
aikenm <a.mnsbkv@gmail.com>
2023-09-07 23:12:22 +0600

    Deploy bug fix

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProfileForm from '../components/ProfileForm';
 
-function ProfilePage({}) {
+function ProfilePage() {
     return (
         <div>
             <ProfileForm />
